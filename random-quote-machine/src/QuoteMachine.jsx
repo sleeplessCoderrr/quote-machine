92c482b2481ad32c63e942c8d6e547077faa9d99
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-10-11 00:05:58 +0700

    finished

--- random-quote-machine/src/QuoteMachine.jsx
+++ random-quote-machine/src/QuoteMachine.jsx
@@ -20,16 +20,16 @@ function QuoteMachine(){
   
   return (
     <div id="quote-box" className="quote-box">
-          <p id="text">{quote}</p>
-          <p id="author">{author}</p>
-          <button id="new-quote" onClick={fetchData}>New Quote</button>
-          <a 
-              id="tweet-quote" 
-              href={`https://twitter.com/intent/tweet?text="${quote}" - ${author}`} 
-              target="_blank" 
-              rel="noopener noreferrer">
-              Tweet Quote
-          </a>
+        <p id="text">"{quote}"</p>
+        <p id="author">- {author}</p>
+        <button id="new-quote" onClick={fetchData}>New Quote</button>
+        <a 
+            id="tweet-quote" 
+            href={`https://twitter.com/intent/tweet?text="${quote}" - ${author}`} 
+            target="_blank" 
+            rel="noopener noreferrer">
+            Tweet Quote
+        </a>
       </div>
   );
 }
